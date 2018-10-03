d8a84b50674e8340c1fbea4888313b9dd90f2ffb
Gregory Jefferis <jefferis@gmail.com>
2018-10-03 13:49:43 +0100

    Add first version of merge table

---
+++ R/data.R
@@ -0,0 +1,7 @@
+#' Data object defining the merge groups for raw segments
+#'
+#' @details The original information was shared by Peter Li in 10 csv files
+#' @docType data
+#' @examples
+#' head(segment_merge_groups.dt)
+"segment_merge_groups.dt"
